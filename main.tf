1a5f9339806015c65c26aed068d83a9bfc975de5
pranjalkumarsadawarte <pranj.sadawarte@gmail.com>
2025-03-05 20:50:18 -0500

    chore: add resource group and storage account

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+    required_providers {
+        azurerm = {
+            source = hashicorp/azurerm
+            version = "~> 4.21.1"
+        }
+        version = ">=1.11.0"
+    }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "az_rg" {
+    name    = "myResourceGroup"
+    location = "East US"
+}
+
+resource "azurerm_storage_account" "az_sa" {
+    name                     = "mystorageaccount"
+    resource_group_name      = azurerm_resource_group.az_rg.name
+    location                 = azurerm_resource_group.az_rg.location
+    account_tier             = "Standard"
+    account_replication_type = "GRS"
+}
